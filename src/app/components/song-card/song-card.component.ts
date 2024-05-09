a71724dbadd63ae14daff04cc12d84219cc00cf7
Samuel Fuentes (Genius) <samuelgenius1034@gmail.com>
2024-05-09 07:40:40 -0400

    Starting the Song Card Component

--- src/app/components/song-card/song-card.component.ts
+++ src/app/components/song-card/song-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-song-card',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './song-card.component.css'
 })
 export class SongCardComponent {
-
+  @Input() public playlistThumbnail!: string;
+  @Input() public title!: string;
+  @Input() public description!: string;
 }
